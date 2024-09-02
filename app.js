49dc7af82c8eeda48ca8ff66b9d9b1a547624168
Bahe <bahe.yaich@live.ca>
2024-09-02 13:28:16 +0300

    Chapter 8.3 : Deployment exercise

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-  return new Response("Hello world!");
+    const url = new URL(request.url);
+  return new Response(url);
 };
 
 serve(handleRequest, { port: 7777 });
