33b292672fd47d85ab986ec966f06e6323eb4994
Bahe <bahe.yaich@live.ca>
2024-09-02 13:54:49 +0300

    Correction

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { serve } from "./deps.js";
 
 const handleRequest = (request) => {
-    const url = new URL(request.url);
-  return new Response(url);
+    let message = "Seeking truths beyond meaning of life, you will find 43.";
+  return new Response(message);
 };
 
 serve(handleRequest, { port: 7777 });
