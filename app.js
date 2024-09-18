00bfead032f2427803a289ecb6c8aeb062da106d
Bahe <bahe.yaich@live.ca>
2024-09-18 21:35:50 +0300

    Adding the walking skeleton from the Aalto course

--- app.js
+++
@@ -1,8 +0,0 @@
-import { serve } from "./deps.js";
-
-const handleRequest = (request) => {
-    let message = "Seeking truths beyond meaning of life, you will find 43.";
-  return new Response(message);
-};
-
-serve(handleRequest, { port: 7777 });
