27baa6474961437ce2a19e7066fc15f198c88668
Bahe <bahe.yaich@live.ca>
2024-10-03 22:58:22 +0300

    Experimenting with GH Actions #8 Updataing bash script to include the correct paths

--- shopping-lists/compile_sass.sh
+++ shopping-lists/compile_sass.sh
@@ -1,14 +1,15 @@
 # shopping-lists/compile_sass.sh
 #!/bin/bash
 
-# Check if the output directory exists, create it if it doesn't
-OUTPUT_DIR="../assets/css"
+echo "Starting SCSS compilation..."
+
+OUTPUT_DIR="../shopping-lists/assets/css"
+echo "Output directory: $OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
-# Compile the SCSS to CSS
-sass ./assets/scss/main.scss "$OUTPUT_DIR/main.min.css" --style compressed
+echo "Compiling SCSS..."
+sass ../shopping-lists/assets/scss/main.scss "$OUTPUT_DIR/main.min.css" --style compressed
 
-# Check for errors during compilation
 if [ $? -eq 0 ]; then
     echo "SCSS compiled successfully!"
 else
