6ab8f08bff773672f295dc338e56d0baad94107b
Bahe <bahe.yaich@live.ca>
2024-10-03 23:12:11 +0300

    Experimenting with GH Actions #10 Checking the paths issue again

--- shopping-lists/compile_sass.sh
+++ shopping-lists/compile_sass.sh
@@ -3,12 +3,14 @@
 
 echo "Starting SCSS compilation..."
 
+INPUT_DIR="assets/scss"
+echo "Input directory: $INPUT_DIR"
 OUTPUT_DIR="assets/css"
 echo "Output directory: $OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
 echo "Compiling SCSS..."
-sass ../shopping-lists/assets/scss/main.scss "$OUTPUT_DIR/main.min.css" --style compressed
+sass "$INPUT_DIR/main.scss" "$OUTPUT_DIR/main.min.css" --style compressed
 
 if [ $? -eq 0 ]; then
     echo "SCSS compiled successfully!"
