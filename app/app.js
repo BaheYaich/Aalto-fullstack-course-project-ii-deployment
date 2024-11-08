47267d1d66d3c9db7b435d4e3bb6dc53e3e88235
Bahe <bahe.yaich@live.ca>
2024-11-08 13:03:09 +0200

    Chapter 25.1 : Authentication with form

--- app/app.js
+++ app/app.js
@@ -1,45 +1,49 @@
-import { Application, Router } from "./deps.js";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { Session } from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
+import { renderMiddleware } from "./middlewares/renderMiddleware.js";
 
 const app = new Application();
 const router = new Router();
+
 app.use(Session.initMiddleware());
+app.use(renderMiddleware);
+
+const authMiddleware = async ({ request, response, state }, next) => {
+  if (request.url.pathname !== '/login' && !(await state.session.get('authenticated'))) {
+    response.redirect('/login')
+  } else {
+    await next()
+  }
+}
+
+const hello = ({ response }) => {
+  response.body = 'Only authenticated users can access this';
+}
+
+const showForm = async ({ render }) => {
+  await render('index.eta')
+}
 
 const authenticate = async ({ request, response, state }) => {
-  const authenticated = await state.session.get("authenticated");
+  const body = request.body({ type: "form" })
+  const params = await body.value
 
-  if (!authenticated) {
-    const body = request.body();
-    const params = await body.value;
-    const username = params.get("username");
-    const password = params.get("password");
-    if ((password === "00000000") && (username === "Minuteman")) {
-      await state.session.set("authenticated", true);
-      response.body = "Authenticated";
-      response.redirect("/");
-    } else {
-      response.status = 401;
-      response.body = "Bad credentials";
-    }
-  };
-}
-
-const showStatus = async ({ response, state }) => {
-  const authenticated = await state.session.get("authenticated")
-  if (!authenticated) {
-    response.body = "Not authenticated";
+  const password = params.get('password')
+  if (password === 'asparagus') {
+    await state.session.set('authenticated', true)
+    response.redirect('/')
   } else {
-    response.body = "Authenticated";
+    response.status = 401
+    response.body = 'Unauthorized: Invalid Password'
   }
-};
-
-router.get("/", showStatus);
-router.post("/", authenticate);
-
-app.use(router.routes());
-
-if (!Deno.env.get("TEST_ENVIRONMENT")) {
-  app.listen({ port: 7777 });
 }
 
-export default app;
+router.get('/', hello)
+router.get('/login', showForm)
+router.post('/login', authenticate)
+
+app.use(authMiddleware)
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen({ port: 7777 })
