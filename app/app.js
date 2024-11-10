0c6b32b6526336bcd5a080e0329e0e8f3aa21669
Bahe <bahe.yaich@live.ca>
2024-11-10 20:45:28 +0200

    Chapter 26.2: Songs and ratings

--- app/app.js
+++ app/app.js
@@ -1,27 +1,60 @@
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { Session } from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
-import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { renderMiddleware } from "./middlewares/renderMiddleware.js";
-import { router } from "./routes/routes.js";
+import { sql } from "./database/database.js";
 
 const app = new Application();
-app.use(errorMiddleware);
-
-app.use(Session.initMiddleware());
-
-app.use(async ({ request, response, state }, next) => {
-  if (request.url.pathname.startsWith("/accounts")) {
-    if (await state.session.get("authenticated")) {
-      await next()
-    } else {
-      response.status = 401
-    }
-  } else {
-    await next()
-  }
-})
+const router = new Router();
 
 app.use(renderMiddleware);
+
+const getSongs = async () => {
+  return await sql`SELECT * FROM songs`;
+};
+
+const addSong = async (name, rating) => {
+  return await sql`INSERT INTO songs (name, rating) VALUES (${name}, ${rating})`;
+};
+
+const showForm = async ({ render }) => {
+  render("index.eta", { songs: await getSongs(), name: "", rating: "" });
+};
+
+
+const submitForm = async ({ request, render, response }) => {
+  const body = request.body();
+  const params = await body.value;
+
+  const name = params.get("name") || "";
+  const ratingStr = params.get("rating") || "";
+  const rating = ratingStr ? Number(ratingStr) : null;
+
+  const errors = [];
+
+  if ((name.length <= 5) || (name.length >= 20)) {
+    errors.push("Song name must be between 5 and 20 characters long");
+  }
+
+  if (ratingStr) {
+    if ((isNaN(rating)) || (rating <= 1 || rating >= 10)) {
+      errors.push("Rating must be a number between 1 and 10");
+    }
+  }
+
+  if (errors.length > 0) {
+    await render("index.eta", {
+      errors: errors,
+      name: name,
+      rating: ratingStr,
+    });
+    return;
+  }
+
+  await addSong(name, rating);
+  response.redirect("/");
+};
+
+router.get("/", showForm);
+router.post("/", submitForm);
 
 app.use(router.routes());
 
--- app/routes/controllers/mainController.js
+++
@@ -1,9 +0,0 @@
-const showMain = async ({ response, render, state }) => {
-  if (await state.session.get("authenticated")) {
-    render("index.eta", await state.session.get("user"));
-  } else {
-    response.redirect("/auth/login");
-  }
-};
-
-export { showMain };
