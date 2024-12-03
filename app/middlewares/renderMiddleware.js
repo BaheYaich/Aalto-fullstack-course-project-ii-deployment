dee58bc5f9783cd210e61df5532af81190f95e86
Bahe <bahe.yaich@live.ca>
2024-12-03 02:13:03 +0200

    Chapter 34: Working application example

--- app/middlewares/renderMiddleware.js
+++ app/middlewares/renderMiddleware.js
@@ -1,10 +1,19 @@
-import { configure, renderFile } from "../deps.js"
+import { configure, renderFile } from "../deps.js";
+
+configure({
+  views: `${Deno.cwd()}/views/`,
+});
 
 const renderMiddleware = async (context, next) => {
-  configure({
-    views: `${Deno.cwd()}/views/`,
-  });
   context.render = async (file, data) => {
+    if (!data) {
+      data = {};
+    }
+
+    if (context.user) {
+      data.user = context.user;
+    }
+
     context.response.headers.set("Content-Type", "text/html; charset=utf-8");
     context.response.body = await renderFile(file, data);
   };
