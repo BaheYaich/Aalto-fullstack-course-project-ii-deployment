561e86961c2b26229bedb7d129576c88386e9465
Bahe <bahe.yaich@live.ca>
2025-01-14 11:50:42 +0200

    Fly.io: #3

--- drill-and-practice/frontend/svelte.config.ts
+++ drill-and-practice/frontend/svelte.config.ts
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,7 @@ const config = {
 	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x',
-			edge: false,
-			split: false,
-			external: [],
+			out: 'build'
 		}),
 		files: {
 			assets: 'static',
